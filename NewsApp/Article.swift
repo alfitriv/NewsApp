0ff561a493ef619e52963b3b08f23fcdf1213c96
Mila Alfitri <alfitriv@gmail.com>
2020-05-29 23:32:12 +0700

    Added kingfisher for image url processing

--- NewsApp/Article.swift
+++ NewsApp/Article.swift
@@ -10,11 +10,11 @@ import Foundation
 
 struct Article: Codable {
     var source: Source
-    var author: String
+    var author: String?
     var title: String
-    var description: String
-    var url: String
-    var urlToImage: String
+    var description: String?
+    var url: String?
+    var urlToImage: String?
     var publishedAt: String
     var content: String?
 }
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NewsApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewsApp
+	pod 'Kingfisher', '~> 5.0'
+
+end
