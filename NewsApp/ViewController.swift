02e9c68eb71d5a001e63f9aab312d118f35c4a41
Mila Alfitri <alfitriv@gmail.com>
2020-05-29 20:44:19 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Vania Radmila Alfitri on 29/05/20.
+//  Copyright © 2020 Vania Radmila Alfitri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
